13f1d5a9c995b00fe5cb054bf080a5fcc6017798
Mateusz <wirus006@gmail.com>
2021-01-10 11:47:16 +0100

    testing observer in react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import React from 'react';
+import React, {Fragment} from 'react';
 
 import View from './components/View';
 import Decorate from './components/Decorate';
@@ -18,19 +18,53 @@ import database from './database.js';
 import Banner from "./components/Banner";
 import Navigation from "./components/Navigation";
 
+
+function useOnScreen(options) {
+    const ref = React.useRef();
+    const [visible, setVisible] = React.useState(false);
+
+    React.useEffect(() => {
+        const observer = new IntersectionObserver(([entry]) => {
+            setVisible(entry.isIntersecting);
+        }, options);
+
+        if (ref.current) {
+            observer.observe(ref.current);
+        }
+
+        return() => {
+            if(ref.current) {
+                observer.unobserve(ref.current);
+            }
+        };
+    }, [ref, options]);
+
+    return [ref, visible];
+}
+
 function App() {
-  return (
+
+    const [ref, visible] = useOnScreen({rootMargin: '-200px'});
+
+    return (
     <div className="App">
-        <Navigation logo={miniLogo} />
         <View>
             <Decorate titleView={true} url={logo} alt="Logo G&M. Podkreślony napis G&M w okręgu."/>
             <Content titleView={true} id={"home"}/>
         </View>
-        <View>
-            <Content titleView={false} header={database.about.header}
-                     headerSpan={database.about.headerSpan} describe={database.about.paragraph} id={"about"}/>
-            <Decorate titleView={false} url={yingYang} alt="Znak równowagi. Okrąg z jasną i ciemną połową, które nachodzą na siebie."/>
-        </View>
+        <div className="observer-div" ref={ref}>
+            { visible ? (
+                    <Navigation logo={miniLogo} linkColor='#171820'/>
+                ) : (
+                    <Navigation logo={miniLogo} linkColor='#D7D7D7'/>
+                )
+            }
+            <View>
+                <Content titleView={false} header={database.about.header}
+                         headerSpan={database.about.headerSpan} describe={database.about.paragraph} id={"about"}/>
+                <Decorate titleView={false} url={yingYang} alt="Znak równowagi. Okrąg z jasną i ciemną połową, które nachodzą na siebie."/>
+            </View>
+        </div>
         <View>
             <Decorate titleView={false} url={monitor} alt="Monitor wyświetlający przykładową stronę www."/>
             <Content titleView={false} header={database.website.header}
@@ -49,7 +83,7 @@ function App() {
         <Banner url={logo} alt="Mniejsze logo głowne." />
         <Contact />
     </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './Navigation.scss';
 
-function Navigation({logo}) {
+function Navigation({logo, linkColor}) {
     return (
         <header className="Navigation">
             <nav className="Navigation__nav">
@@ -13,34 +13,34 @@ function Navigation({logo}) {
                 <div className="Navigation__right">
                     <ul className="Navigation__list">
                         <li className="Navigation__item">
-                            <a href="#about" className="Navigation__link">
+                            <a href="#about" className="Navigation__link" style={{color: linkColor}}>
                                 O NAS
                             </a>
                         </li>
                         <li className="Navigation__item Navigation__item--offer">
-                            <a href="#home" className="Navigation__link">
+                            <a href="#home" className="Navigation__link" style={{color: linkColor}}>
                                 OFERTA
                             </a>
                             <ul className="Navigation__sublist">
                                 <li className="Navigation__item Navigation__item--subitem">
-                                    <a href="#websites" className="Navigation__link Navigation__link--sublink">
+                                    <a href="#websites" className="Navigation__link Navigation__link--sublink" style={{color: {linkColor}}}>
                                         STRONY INTERNETOWE
                                     </a>
                                 </li>
                                 <li className="Navigation__item Navigation__item--subitem">
-                                    <a href="#logotypes" className="Navigation__link Navigation__link--sublink">
+                                    <a href="#logotypes" className="Navigation__link Navigation__link--sublink" style={{color: {linkColor}}}>
                                         LOGOTYPY
                                     </a>
                                 </li>
                                 <li className="Navigation__item Navigation__item--subitem">
-                                    <a href="#uav" className="Navigation__link Navigation__link--sublink">
+                                    <a href="#uav" className="Navigation__link Navigation__link--sublink" style={{color: {linkColor}}}>
                                         NAGRYWANIE DRONEM
                                     </a>
                                 </li>
                             </ul>
                         </li>
                         <li className="Navigation__item">
-                            <a href="#contact" className="Navigation__link">
+                            <a href="#contact" className="Navigation__link" style={{color: {linkColor}}}>
                                 KONTAKT
                             </a>
                         </li>

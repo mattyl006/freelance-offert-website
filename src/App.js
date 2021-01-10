0efd6d49a1909c0922f1e8257474f827fb93cbca
Mateusz <wirus006@gmail.com>
2021-01-10 14:49:07 +0100

    refactor useOnScreen hook to fix warning

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import Navigation from "./components/Navigation";
 
 
 function useOnScreen(options) {
-    const ref = React.useRef();
+    const [ref, setRef] = React.useState(null);
     const [visible, setVisible] = React.useState(false);
 
     React.useEffect(() => {
@@ -28,34 +28,34 @@ function useOnScreen(options) {
             setVisible(entry.isIntersecting);
         }, options);
 
-        if (ref.current) {
-            observer.observe(ref.current);
+        if (ref) {
+            observer.observe(ref);
         }
 
         return() => {
-            if(ref.current) {
-                observer.unobserve(ref.current);
+            if(ref) {
+                observer.unobserve(ref);
             }
         };
     }, [ref, options]);
 
-    return [ref, visible];
+    return [setRef, visible];
 }
 
 function App() {
 
-    const [refHome, visibleHome] = useOnScreen({rootMargin: '-76px'});
-    const [refAbout, visibleAbout] = useOnScreen({rootMargin: '-76px'});
-    const [refWebsites, visibleWebsites] = useOnScreen({rootMargin: '-76px'});
-    const [refLogotypes, visibleLogotypes] = useOnScreen({rootMargin: '-76px'});
-    const [refUav, visibleUav] = useOnScreen({rootMargin: '-76px'});
-    const [refContact, visibleContact] = useOnScreen({rootMargin: '-76px'});
+    const [setRefHome, visibleHome] = useOnScreen({rootMargin: '-76px'});
+    const [setRefAbout, visibleAbout] = useOnScreen({rootMargin: '-76px'});
+    const [setRefWebsites, visibleWebsites] = useOnScreen({rootMargin: '-76px'});
+    const [setRefLogotypes, visibleLogotypes] = useOnScreen({rootMargin: '-76px'});
+    const [setRefUav, visibleUav] = useOnScreen({rootMargin: '-76px'});
+    const [setRefContact, visibleContact] = useOnScreen({rootMargin: '-76px'});
     const darkColor = '#171820';
     const lightColor = '#D7D7D7';
     const darkColorBackground = 'rgba(23, 24, 32, 0.8)';
     const lightColorBackground = 'rgba(215, 215, 215, 0.8)';
 
-    function observerHelper(visibleHome, visibleAbout, visibleWebsites, visibleLogotypes, visibleUav, visibleContact) {
+    function observerNavigation(visibleHome, visibleAbout, visibleWebsites, visibleLogotypes, visibleUav, visibleContact) {
         if(!visibleHome && visibleAbout) {
             // return ("lewa ciemna, prawa jasna, napisy ciemne");
             return (
@@ -100,42 +100,42 @@ function App() {
 
     return (
     <div className="App">
-        {observerHelper(visibleHome, visibleAbout, visibleWebsites, visibleLogotypes, visibleUav, visibleContact)}
-        <div className="observer-div" ref={refHome}>
+        {observerNavigation(visibleHome, visibleAbout, visibleWebsites, visibleLogotypes, visibleUav, visibleContact)}
+        <div className="observer-div" ref={setRefHome}>
             <View>
                 <Decorate titleView={true} url={logo} alt="Logo G&M. Podkreślony napis G&M w okręgu."/>
                 <Content titleView={true} id={"home"}/>
             </View>
         </div>
-        <div className="observer-div" ref={refAbout}>
+        <div className="observer-div" ref={setRefAbout}>
             <View>
                 <Content titleView={false} header={database.about.header}
                          headerSpan={database.about.headerSpan} describe={database.about.paragraph} id={"about"}/>
                 <Decorate titleView={false} url={yingYang} alt="Znak równowagi. Okrąg z jasną i ciemną połową, które nachodzą na siebie."/>
             </View>
         </div>
-        <div className="observer-div" ref={refWebsites}>
+        <div className="observer-div" ref={setRefWebsites}>
             <View>
                 <Decorate titleView={false} url={monitor} alt="Monitor wyświetlający przykładową stronę www."/>
                 <Content titleView={false} header={database.website.header}
                          headerSpan={database.website.headerSpan} describe={database.website.paragraph} id={"websites"}/>
             </View>
         </div>
-        <div className="observer-div" ref={refLogotypes}>
+        <div className="observer-div" ref={setRefLogotypes}>
             <View>
                 <Content titleView={false} header={database.logotypes.header}
                          headerSpan={database.logotypes.headerSpan} describe={database.logotypes.paragraph} id={"logotypes"}/>
                 <Decorate titleView={false} url={exampleLogo} alt="Przykładowe logo zaprojektowane przez Grzegorza."/>
             </View>
         </div>
-        <div className="observer-div" ref={refUav}>
+        <div className="observer-div" ref={setRefUav}>
             <View>
                 <Video url={dji}/>
                 <Content titleView={false} header={database.uav.header}
                          headerSpan={database.uav.headerSpan} describe={database.uav.paragraph} id={"uav"}/>
             </View>
         </div>
-        <div className="observer-div" ref={refContact}>
+        <div className="observer-div" ref={setRefContact}>
             <Banner url={logo} alt="Mniejsze logo głowne." />
             <Contact />
         </div>

2110b963f0b702ea8727bddf9e59887bed442bcd
Mateusz <wirus006@gmail.com>
2021-01-25 21:00:43 +0100

    fix: correct statement block in Decorate component

--- src/components/sections/Decorate/Decorate.js
+++ src/components/sections/Decorate/Decorate.js
@@ -6,9 +6,16 @@ import Header from "../../common/Header";
 
 
 function Decorate({titleView, mobileView, url, alt}) {
-    if(titleView === true) {
+    if(titleView === true && mobileView === true) {
         return (
             <div className="Decorate" style={{height:'100vh'}}>
+                <img className="Decorate__image" src={url} alt={alt}/>
+                <Header/>
+            </div>
+        );
+    } else if(titleView === true) {
+        return (
+            <div className="Decorate" >
                 <img className="Decorate__image" src={url} alt={alt}/>
                 <Header/>
             </div>

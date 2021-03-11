0e761a790bdf091eed1d50bb6e6c6dcff7f9756c
Mateusz <wirus006@gmail.com>
2021-03-11 23:50:57 +0100

    LogotypesMobile implemented

--- src/pages/Logotypes/LogotypesMobile/LogotypesMobile.js
+++ src/pages/Logotypes/LogotypesMobile/LogotypesMobile.js
@@ -1,11 +1,31 @@
 import React from "react";
 import './LogotypesMobile.scss';
 import ExamplesLogotypes from "../../../components/sections/ExamplesLogotypes";
+import MobileNav from "../../../components/common/MobileNav";
+import Decorate from "../../../components/sections/Decorate";
+import exampleLogo from "../../../assets/your-logo.svg";
+import Content from "../../../components/sections/Content";
+import database from "../../../database";
+import Packages from "../../../components/sections/Packages";
+import MobileFooter from "../../../components/common/MobileFooter/MobileFooter";
 
 function LogotypesMobile() {
+
+    React.useLayoutEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <div className="LogotypesMobile">
+            <MobileNav />
+            <Decorate titleView={false} mobileView={true} url={exampleLogo}
+                      alt="Przykładowe logo zaprojektowane przez Grzegorza." id={"logotypes"}/>
+            <Content titleView={false} header={database.logotypes.header} button={false}
+                     headerSpan={database.logotypes.headerSpan}
+                     describe={database.logotypes.more}/>
+             <Packages section={"logotypes"} />
             <ExamplesLogotypes />
+            <MobileFooter />
         </div>
     );
 }

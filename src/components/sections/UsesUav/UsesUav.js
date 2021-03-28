637d0a6eb88de7a2d3492ec4c06247e1723f9e0b
Mateusz <wirus006@gmail.com>
2021-03-28 18:21:09 +0200

    UsesUav component styles correct to desktop also

--- src/components/sections/UsesUav/UsesUav.js
+++ src/components/sections/UsesUav/UsesUav.js
@@ -7,81 +7,83 @@ function UsesUav() {
             <h2 className="UsesUav__header">
                 Gdzie możesz wykorzystać filmy i ujęcia z drona?
             </h2>
-            <article className="UsesUav__article">
-                <h3 className="UsesUav__article-header">
-                    FILMY PROMOCYJNE LUB
-                    REKLAMOWE
-                </h3>
-                <ul className="UsesUav__list">
-                    <li className="UsesUav__item">
-                        Reklamy dla firm
-                    </li>
-                    <li className="UsesUav__item">
-                        Promocje wydarzeń
-                    </li>
-                    <li className="UsesUav__item">
-                        Promocje miast
-                    </li>
-                    <li className="UsesUav__item">
-                        ITD.
-                    </li>
-                </ul>
-            </article>
-            <article className="UsesUav__article">
-                <h3 className="UsesUav__article-header">
-                    OGŁOSZENIA
-                    SPRZEDAŻOWE
-                </h3>
-                <ul className="UsesUav__list">
-                    <li className="UsesUav__item">
-                        Nieruchomości
-                    </li>
-                    <li className="UsesUav__item">
-                        Samochodów
-                    </li>
-                    <li className="UsesUav__item">
-                        Maszyn
-                    </li>
-                    <li className="UsesUav__item">
-                        ITD.
-                    </li>
-                </ul>
-            </article>
-            <article className="UsesUav__article">
-                <h3 className="UsesUav__article-header">
-                    ZDJĘCIA Z POWIETRZA
-                </h3>
-                <ul className="UsesUav__list">
-                    <li className="UsesUav__item">
-                        Nieruchomości
-                    </li>
-                    <li className="UsesUav__item">
-                        Działek
-                    </li>
-                    <li className="UsesUav__item">
-                        Firm
-                    </li>
-                    <li className="UsesUav__item">
-                        ITD.
-                    </li>
-                </ul>
-            </article>
-            <article className="UsesUav__article">
-                <h3 className="UsesUav__article-header">
-                    RELACJE Z WYDARZEŃ
-                </h3>
-                <ul className="UsesUav__list">
-                    <li className="UsesUav__item">
-                        Wesela
-                    </li>
-                    <li className="UsesUav__item">
-                        Imprezy okolicznościowe
-                    </li>
-                    <li className="UsesUav__item">
-                        ITD.
-                    </li>
-                </ul>
-            </article>
+            <div className="UsesUav__container">
+                <article className="UsesUav__article">
+                    <h3 className="UsesUav__article-header">
+                        FILMY PROMOCYJNE LUB
+                        REKLAMOWE
+                    </h3>
+                    <ul className="UsesUav__list">
+                        <li className="UsesUav__item">
+                            Reklamy dla firm
+                        </li>
+                        <li className="UsesUav__item">
+                            Promocje wydarzeń
+                        </li>
+                        <li className="UsesUav__item">
+                            Promocje miast
+                        </li>
+                        <li className="UsesUav__item">
+                            ITD.
+                        </li>
+                    </ul>
+                </article>
+                <article className="UsesUav__article">
+                    <h3 className="UsesUav__article-header">
+                        OGŁOSZENIA
+                        SPRZEDAŻOWE
+                    </h3>
+                    <ul className="UsesUav__list">
+                        <li className="UsesUav__item">
+                            Nieruchomości
+                        </li>
+                        <li className="UsesUav__item">
+                            Samochodów
+                        </li>
+                        <li className="UsesUav__item">
+                            Maszyn
+                        </li>
+                        <li className="UsesUav__item">
+                            ITD.
+                        </li>
+                    </ul>
+                </article>
+                <article className="UsesUav__article">
+                    <h3 className="UsesUav__article-header">
+                        ZDJĘCIA Z POWIETRZA
+                    </h3>
+                    <ul className="UsesUav__list">
+                        <li className="UsesUav__item">
+                            Nieruchomości
+                        </li>
+                        <li className="UsesUav__item">
+                            Działek
+                        </li>
+                        <li className="UsesUav__item">
+                            Firm
+                        </li>
+                        <li className="UsesUav__item">
+                            ITD.
+                        </li>
+                    </ul>
+                </article>
+                <article className="UsesUav__article">
+                    <h3 className="UsesUav__article-header">
+                        RELACJE Z WYDARZEŃ
+                    </h3>
+                    <ul className="UsesUav__list">
+                        <li className="UsesUav__item">
+                            Wesela
+                        </li>
+                        <li className="UsesUav__item">
+                            Imprezy okolicznościowe
+                        </li>
+                        <li className="UsesUav__item">
+                            ITD.
+                        </li>
+                    </ul>
+                </article>
+            </div>
         </section>
     );
 }

99da75cedf19bf8b1c9832c7ec72ac01705cf82a
Mateusz <wirus006@gmail.com>
2021-04-02 20:03:54 +0200

    corrections: Mobile Title View and Navigation margin-right

--- src/components/sections/Decorate/Decorate.js
+++ src/components/sections/Decorate/Decorate.js
@@ -6,7 +6,7 @@ import Header from "../../common/Header";
 function Decorate({titleView, mobileView, uavImages, url, url2, alt, alt2, id}) {
     if(titleView === true && mobileView === true) {
         return (
-            <div className="Decorate" style={{height:'60vh'}} id={id}>
+            <div className="Decorate" style={{height:'50vh'}} id={id}>
                 <img className="Decorate__image" src={url} alt={alt}/>
                 <Header/>
             </div>

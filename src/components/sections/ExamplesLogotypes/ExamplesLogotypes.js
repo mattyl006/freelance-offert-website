ca2a43c24e8afffe61826392667a3e069c47320b
Mateusz <wirus006@gmail.com>
2021-04-04 12:56:32 +0200

    add h2 to ExamplesLogotypes component

--- src/components/sections/ExamplesLogotypes/ExamplesLogotypes.js
+++ src/components/sections/ExamplesLogotypes/ExamplesLogotypes.js
@@ -10,21 +10,26 @@ import safeHouseGold from "../../../assets/safe-house-gold.svg";
 function ExamplesLogotypes() {
     return (
         <div className="ExamplesLogotypes">
-            <div className="ExamplesLogotypes__side">
-                <img className="ExamplesLogotypes__logo" src={safeHouse}
-                     alt={"Przykładowe logo, czarna chatka z podpisem safe house po prawej."}/>
-                <img className="ExamplesLogotypes__logo" src={newName}
-                     alt={"Przykładowe logo, niebieski napis new name."}/>
-            </div>
-            <div className="ExamplesLogotypes__middle">
-                <img className="ExamplesLogotypes__logo" src={yourLogoDark}
-                     alt={"Przykładowe logo, napis your logo z podpisem your company name pod sobą, po lewej okrągły wzór ze strzałkami, tło jasne"}/>
-            </div>
-            <div className="ExamplesLogotypes__side">
-                <img className="ExamplesLogotypes__logo" src={yourLogoLight}
-                     alt={"Przykładowe logo, napis your logo z podpisem your company name pod sobą, po lewej okrągły wzór ze strzałkami, tło ciemne"}/>
-                <img className="ExamplesLogotypes__logo ExamplesLogotypes__logo--gold" src={safeHouseGold}
-                     alt={"Przykładowe logo, złota chatka w złotej tarczy z podpisem safe house pod sobą."}/>
+            <h2 className="ExamplesLogotypes__header">
+                Przykłady logotypów stworzone przez nas
+            </h2>
+            <div className="ExamplesLogotypes__container">
+                <div className="ExamplesLogotypes__side">
+                    <img className="ExamplesLogotypes__logo" src={safeHouse}
+                         alt={"Przykładowe logo, czarna chatka z podpisem safe house po prawej."}/>
+                    <img className="ExamplesLogotypes__logo" src={newName}
+                         alt={"Przykładowe logo, niebieski napis new name."}/>
+                </div>
+                <div className="ExamplesLogotypes__middle">
+                    <img className="ExamplesLogotypes__logo" src={yourLogoDark}
+                         alt={"Przykładowe logo, napis your logo z podpisem your company name pod sobą, po lewej okrągły wzór ze strzałkami, tło jasne"}/>
+                </div>
+                <div className="ExamplesLogotypes__side">
+                    <img className="ExamplesLogotypes__logo" src={yourLogoLight}
+                         alt={"Przykładowe logo, napis your logo z podpisem your company name pod sobą, po lewej okrągły wzór ze strzałkami, tło ciemne"}/>
+                    <img className="ExamplesLogotypes__logo ExamplesLogotypes__logo--gold" src={safeHouseGold}
+                         alt={"Przykładowe logo, złota chatka w złotej tarczy z podpisem safe house pod sobą."}/>
+                </div>
             </div>
         </div>
     );
